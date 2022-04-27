771e5fdfe544b6a31ade1d432eb77ded94d56d83
Charanvir <charanvir123@gmail.com>
2022-04-27 03:24:57 -0400

    connected database with node.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require("express");
+const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,22 +7,28 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: '127.0.0.1',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Capsfan123!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
-
-
-
-
-
-
-
-
-// Default response for any other request (NOT FOUND)
+// Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
-})
-
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
