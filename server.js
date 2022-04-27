dd9c84a18dd43d0eba686fc768bbfec087287b69
Charanvir <charanvir123@gmail.com>
2022-04-27 03:56:16 -0400

    implemented a get request for a single candidate

--- server.js
+++ server.js
@@ -20,18 +20,38 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-// // Gets all the candidates from the database
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
+// Gets all the candidates from the database
+app.get("/api/candidates", (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
 // Get a single candidate from the database
-db.query(`SELECT * FROM candidates WHERE id = 11`, (err, row) => {
-    if (err) {
-        console.log(err)
-    }
-    console.log(row)
-})
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message })
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
 
 // // Delete a candidate
 // db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
